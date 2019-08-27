8163702e8d25e6250aa4f07fe05ef6772fb14264
clobotics_ccp <>
2019-08-27 13:28:15 +0800

    install

---
+++ CCPDate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CCPDate
+//
+//  Created by clobotics_ccp on 2019/8/27.
+//  Copyright © 2019 cool-ccp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CCPDateExtension
+# CCPDateExtension(swift5.0, iOS10.0+, v1.0.0)
 Date常用的扩展
